53be24eca4d4e79f0aa002fa27812c51e77a2677
Sergio <51914183+Canadaman@users.noreply.github.com>
2021-01-13 23:00:43 +0500

    Update HW2_3_1.sql

--- HW2_3_1.sql
+++ HW2_3_1.sql
@@ -1,5 +1,5 @@
 
-Select  o.OrderID, FORMAT (O.OrderDate, 'MMMM' ) AS [Month], DATEPART(QUARTER, O.OrderDate) AS [Quarter],
+Select  o.OrderID,convert(nvarchar(16), o.OrderDate, 104) as [Orderdate], FORMAT (O.OrderDate, 'MMMM' ) AS [Month], DATEPART(QUARTER, O.OrderDate) AS [Quarter],
 CASE
         WHEN MONTH(o.OrderDate) BETWEEN 1  AND 4  THEN 1
         WHEN MONTH(o.OrderDate) BETWEEN 5  AND 8  THEN 2
@@ -10,5 +10,6 @@ FROM Sales.Orders AS O
 INNER JOIN Sales.OrderLines AS OL ON OL.OrderID=O.OrderID
 left join Sales.Customers AS SC on o.CustomerID=sc.CustomerID
 WHERE O.PickingCompletedWhen IS NOT NULL and (OL.UnitPrice > 100 OR (OL.Quantity>20))
+Order by [Quarter], [Third], o.OrderDate
 
 
